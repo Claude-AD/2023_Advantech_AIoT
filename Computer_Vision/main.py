192204e7f2b88f7d7ff373926e6d53584c61a516
HyunSon <aa000098@naver.com>
2023-11-07 16:10:10 +0900

    fix: minsub vision

--- Computer_Vision/main.py
+++ Computer_Vision/main.py
@@ -1,72 +1,22 @@
-import cv2
-from ultralytics import YOLO
-from Crash import crash
-from collections import deque
+from Vision import process_frame
+from datahub.Data import receive_plag_data
+from datahub.EdgeAgent import generate_edgeAgent, sendDataToDataHub
 
-# video = './dataset/test/part1.mp4'
-video = './dataset/test/cctv.gif'
-cap = cv2.VideoCapture(video)
+def main():
+    edge_agent = generate_edgeAgent()  # EdgeAgent 인스턴스 생성 및 설정
 
-model = YOLO('./model/yolov8n.pt')
+    while True:
+        img, plag = process_frame()  # img와 plag 혹은 img와 None을 받음
 
-previous = []
-sending = deque([])
-accident_video = []
-cnt = 0
+        if img is None and plag is None: # img, plag 둘 다 None이면 영상이 끝난것이니 종료
+            print("No more frames to process or unable to open video. Exiting.")
+            break
 
-if cap.isOpened():
-  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-  fps = int(cap.get(cv2.CAP_PROP_FPS))
-  w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-  h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-  # out = cv2.VideoWriter('1.mp4', fourcc, fps, (w, h))
-  
-  while True:
-    
-    ret, frame = cap.read()
-    if ret == 0 or (cv2.waitKey(1) & 0xFF == ord('q')):
-      break
-    
-    results = model.track(frame, persist=True, verbose=False)
-    img = results[0].plot()
-    white, previous, is_accident, plag = crash(results[0].boxes, img.shape, previous)
-    
-    img = cv2.addWeighted(img, 1, white, 1, 0)
-    cv2.imshow('img', img)
-    # out.write(img)
-    
-    
-    sending.append([img])
-    '''sending(이미지) 보내기'''
-    
-    if is_accident:
-      '''plag 보내기'''
-      cnt = 1
-      accident_video.extend([*sending])  
-    
-    FRAME_LEN = 35
-    if cnt > 0:
-      cnt += 1
-      accident_video.append([img])
-    
-    if cnt >= FRAME_LEN:
-      cnt = 0
-      ########
-      for i in range(len(accident_video)):
-        cv2.imshow('img', accident_video[i][0])
-        if ret == 0 or (cv2.waitKey(1) & 0xFF == ord('q')):
-          break
-      ########
-      
-      '''accident_video 보내기'''
-      accident_video.clear()
-      
-    if len(sending) > FRAME_LEN:
-      sending.popleft()
-      
-  # pd_data = pd.DataFrame(data)
-  # pd_data.to_csv('test.csv')
-  # out.release()
+        if plag is not None: # plag가 None이 아닌 것이 리턴된거면 is_accident이니 처리
+            # 사고가 감지되었을 때 처리
+            print(f"Accident detected, processing plag data: {plag}") # 요 출력문 나오면서 plag 처리함
+            edge_data = receive_plag_data(plag)  # Data.py를 통해 EdgeData로 변환
+            sendDataToDataHub(edge_agent, edge_data)  # 변환된 EdgeData를 EdgeAgent.py를 통해 데이터 허브로 전송
 
-else:
-  print("unable to use camera")
+if __name__ == "__main__":
+    main()
