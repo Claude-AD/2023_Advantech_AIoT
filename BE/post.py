37312bee6b756cb02a8be118f2c96839754334a1
HyunSon <aa000098@naver.com>
2023-11-07 10:16:18 +0900

    fix: more simple and faster

--- BE/post.py
+++ BE/post.py
@@ -4,30 +4,33 @@ import os
 import time
 import cv2
 
-def upload_image(file, bucket, s3_file):
+folder_path = './BE/Accident'
+    
+bucket = 'son-hyunho-icn-reviewweb-bucket'
+s3_filename = 'cctv'
+image_key = f'videos/{s3_filename}'
+image_url = f'https://{bucket}.s3.amazonaws.com/{image_key}'
+
+session = boto3.Session()
+s3 = session.resource('s3')
+s3_obj = s3.Object(bucket, image_key)
+
+def resize_image(file):
     img = cv2.imread(file)
-    res = cv2.resize(img, dsize=(400, 300), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('resized_image.jpg', res)
-    resized_image = './resized_image.jpg'
-    with open(resized_image, 'rb') as file_data:
-        image_key = f'videos/{s3_file}'
-        s3_obj = s3.Object(bucket, image_key)
-        s3_obj.put(Body = file_data, ContentType='image/jpeg')
-    image_url = f'https://{bucket}.s3.amazonaws.com/{image_key}'
+    res = cv2.resize(img, dsize=(600, 400), interpolation=cv2.INTER_CUBIC)
+    image_bytes = cv2.imencode('.jpg', res)[1].tobytes()
+    return image_bytes
+
+def upload_image(file):
+    image_bytes = resize_image(file)
+    s3_obj.put(Body = image_bytes, ContentType='image/jpeg')
     print(image_url)
-    time.sleep(1)
 
 if __name__ == '__main__':
-    folder_path = '../BE/data'
-    
-    session = boto3.Session()
-    s3 = session.resource('s3')
-    bucket_name = 'son-hyunho-icn-reviewweb-bucket'
-    s3_file = 'cctv'
-    
     while(True):
         for filename in os.listdir(folder_path):
             if filename.endswith('.jpg'):
                 file = os.path.join(folder_path, filename)
-                upload_image(file, bucket_name, s3_file)
+                upload_image(file)
+                time.sleep(1)
                 
